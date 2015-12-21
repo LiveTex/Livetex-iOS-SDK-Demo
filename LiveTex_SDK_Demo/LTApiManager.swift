02209c1fdf67e5ed9de70415a4d4517bb12eb6e6
Эмиль Абдуселимов <emil.a@mac-mini-livetex.local>
2015-12-21 16:13:12 +0300

    [ + ] - теперь значения цепляются из настроек приложения(Settings.bundle)

--- LiveTex_SDK_Demo/LTApiManager.swift
+++ LiveTex_SDK_Demo/LTApiManager.swift
@@ -7,13 +7,17 @@
 //
 
 import Foundation
-
+/*
 var URL:String? = "http://authentication-service-sdk-production-1.livetex.ru" //"http://authentication-service.livetex.omnitest:80/" //"http://authentication-service-sdk-production-1.livetex.ru"//"http://authentication-service.livetex.omnitest:80/" //"http://authentication-service-sdk-production-1.livetex.ru"  //"http://authentication-service-sdk-prerelease.livetex.ru" //"http://authentication-service.livetex.omnibuild:80/" //"http://authentication-service-sdk-production-1.livetex.ru"
 var key:String? =  "demo" //"mgvoronin.sbt@sberbank.ru" //"demo" //"dev_key_test" //"demo" // "dev_key_test" //"demo" //"prerelease_key" //"dev_key_test" //"demo"
-var siteId:String? = "106217" //"49134" //"106217"//"106204" //"106217" //"10009775" //"98527" //"10009747" //"106217" //"98527" //"92941" //"10009747" //99878 //98527 Юра
-var offlineDeparmentId:String? = "42" //"72879" //"42" //"10246" //"42" //"117168" //"42" // 12448 // "10246"
+var siteId:String? = "111219" //"49134" //"106217"//"106204" //"106217" //"10009775" //"98527" //"10009747" //"106217" //"98527" //"92941" //"10009747" //99878 //98527 Юра
+var offlineDeparmentId:String? = "119425" //"72879" //"42" //"10246" //"42" //"117168" //"42" // 12448 // "10246"
+*/
 
-
+var URL: String? = NSUserDefaults.standardUserDefaults().stringForKey("LivetexURL")
+var key: String? = NSUserDefaults.standardUserDefaults().stringForKey("LivetexKey")
+var siteID: String? = NSUserDefaults.standardUserDefaults().stringForKey("LivetexSiteID")
+var offlineDepID: String? = NSUserDefaults.standardUserDefaults().stringForKey("LivetexDepID")
 
 class LTApiManager {
     
