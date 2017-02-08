8b18778f58fed450e0e9a5a174f5037cd763d5cd
AEA1986 <emil6olb@gmail.com>
2017-02-08 13:57:03 +0300

    [ ! ] - убраны комментарии

--- LiveTex_SDK_Demo/LivetexCoreManager.swift
+++ LiveTex_SDK_Demo/LivetexCoreManager.swift
@@ -9,9 +9,9 @@
 import Foundation
 import LivetexCore
 
-var URL:String? = "https://authentication-service-sdk-production-1.livetex.ru" //"http://authentication-service.livetex.omnitest:80/" //"http://authentication-service-sdk-production-1.livetex.ru"//"http://authentication-service.livetex.omnitest:80/" //"http://authentication-service-sdk-production-1.livetex.ru"  //"http://authentication-service-sdk-prerelease.livetex.ru" //"http://authentication-service.livetex.omnibuild:80/" //"http://authentication-service-sdk-production-1.livetex.ru"
-var key:String? =  "demo" //"mgvoronin.sbt@sberbank.ru" //"demo" //"dev_key_test" //"demo" // "dev_key_test" //"demo" //"prerelease_key" //"dev_key_test" //"demo"
-var siteID:String? = "123280" //"49134" //"106217"//"106204" //"106217" //"10009775" //"98527" //"10009747" //"106217" //"98527" //"92941" //"10009747" //99878 //98527 Юра
+var URL:String? = "https://authentication-service-sdk-production-1.livetex.ru"
+var key:String? =  "demo"
+var siteID:String? = "123280"
 
 class LivetexCoreManager {
     static let defaultManager: LivetexCoreManager = LivetexCoreManager()
