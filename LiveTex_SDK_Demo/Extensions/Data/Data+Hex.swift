94918f9b6f31eee86a986b5f4028e79270b58c57
dev <dev@livetexsdk.local>
2019-11-14 15:22:03 +0300

    Fix Data+Hex.swift

--- LiveTex_SDK_Demo/Extensions/Data/Data+Hex.swift
+++ LiveTex_SDK_Demo/Extensions/Data/Data+Hex.swift
@@ -11,7 +11,7 @@ import UIKit
 extension Data {
 
     var hexString: String {
-        map { String(format: "%02.2hhx", $0) }.joined()
+        return map { String(format: "%02.2hhx", $0) }.joined()
     }
 
 }
